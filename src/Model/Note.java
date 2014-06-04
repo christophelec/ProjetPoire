a14bd0b85c492b13e59ba70b778dba9807ea71d3
Christophe Lecointe <lecoin_c@epitech.eu>
2014-06-04 21:51:56 +0900

    Added basic note object

---
+++ src/Model/Note.java
@@ -0,0 +1,67 @@
+package Model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chris on 04/06/14.
+ * Used on ${PROJECT_NAME}
+ * Description :
+ * Basic class to represent a Note. The actual content of the note is stored in the file at file_name.
+ */
+public class Note implements Serializable {
+    List<String> tags;
+    String file_name;
+    long last_modified;
+    String title;
+
+    private Note() {
+    }
+
+    public Note(String n_title) {
+        last_modified = new java.util.Date().getTime();
+        title = n_title;
+        tags = new ArrayList<>();
+        file_name = String.valueOf(last_modified) + title;
+    }
+
+    public Note(List<String> n_tags, String n_file_name, long n_last_modified, String n_title) {
+        tags = n_tags;
+        file_name = n_file_name;
+        title = n_title;
+        last_modified = n_last_modified;
+    }
+
+    public List<String> get_tags() {
+        return tags;
+    }
+
+    public void add_tag(String new_tag) {
+        tags.add(new_tag);
+    }
+
+    public void remove_tag(String new_tag) {
+        tags.remove(new_tag);
+    }
+
+    public long get_last_modified() {
+        return last_modified;
+    }
+
+    public void change_last_modified() {
+        last_modified = new java.util.Date().getTime();
+    }
+
+    public String get_file_name() {
+        return file_name;
+    }
+
+    public String get_title() {
+        return title;
+    }
+
+    public void set_title(String n_title) {
+        title = n_title;
+    }
+}
---
+++ src/Model/Opened_note.java
@@ -0,0 +1,38 @@
+package Model;
+
+import java.io.Serializable;
+
+/**
+ * Created by chris on 04/06/14.
+ * Used on ${PROJECT_NAME}
+ * Description :
+ * This class describe a note that has been opened. It is serialized in an other file than the note list (all Note are
+ * serialized in the same file, but they do not contain text)
+ */
+public class Opened_note implements Serializable {
+    Note note;
+    boolean change_saved;
+    String text;
+
+    public void change_text(String n_text) {
+        text = n_text;
+        change_saved = false;
+        note.change_last_modified();
+    }
+
+    public boolean is_saved() {
+        return change_saved;
+    }
+
+    public void save() {
+        change_saved = true;
+    }
+
+    public Note get_note() {
+        return note;
+    }
+
+    public String get_text() {
+        return text;
+    }
+}
