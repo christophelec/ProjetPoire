2ed32cba2886d45eb30b605c87be1b4c3cbc4752
Christophe Lecointe <lecoin_c@epitech.eu>
2014-06-04 21:37:23 +0900

    Deleted old files

--- src/Models/INoteEditor.java
+++
@@ -1,8 +0,0 @@
-package Models;
-
-/**
- * Created by gossel_c on 04/06/2014.
- */
-
-public interface INoteEditor {
-}
--- src/poire/Main.java
+++ src/poire/Main.java
@@ -1,4 +1,4 @@
-package Poire;
+package poire;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
